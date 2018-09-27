0915814de350b64094a82dcc7a3f855e50c26831
SaiKUndurthi <sai.undurthi@gmail.com>
2018-09-26 23:09:31 -0700

    Creating a spring boot application.

---
+++ src/main/java/com/boot/dosboot/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.boot.dosboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "Das Boot, reporting for duty!";
+    }
+}
